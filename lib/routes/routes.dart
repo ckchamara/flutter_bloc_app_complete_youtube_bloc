39085d42030818a781dfdd96cc3b75c53522f414
ckchamara <ckchamara22@gmail.com>
2023-03-21 22:40:14 +0530

    final poits

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import '../screens/editWorkoutScreen.dart';
 import '../screens/homeScreen.dart';
+import '../screens/workout_in_progress_screen.dart';
 
 Map<String, WidgetBuilder> routes = {
-  '/': (context) => const HomeScreen(),
+  // '/': (context) => const HomeScreen(),
+  '/': (context) => const WorkoutInProgressScreen(),
   '/editWorkoutScreen': (context) => const EditWorkoutScreen(),
 };
---
+++ lib/screens/workout_in_progress_screen.dart
@@ -0,0 +1,97 @@
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_app_complete/blocs/workoutBloc/workout_bloc.dart';
+import 'package:flutter_bloc_app_complete/blocs/workoutBloc/workout_bloc.dart';
+
+import '../blocs/helpers/helper.dart';
+
+class WorkoutInProgressScreen extends StatefulWidget {
+  const WorkoutInProgressScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WorkoutInProgressScreen> createState() =>
+      _WorkoutInProgressScreenState();
+}
+
+class _WorkoutInProgressScreenState extends State<WorkoutInProgressScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<WorkoutBloc, WorkoutState>(
+        builder: (context, state) {
+          return Scaffold(
+              appBar: AppBar(
+                title: Text("state.workout!.title.toString()"),
+                leading: BackButton(
+                    // onPressed: () =>
+                    //     BlocProvider.of<WorkoutBloc>(context).goHome(),
+                    ),
+              ),
+              body: Container(
+                padding: const EdgeInsets.all(32),
+                child: Column(
+                  children: [
+                    LinearProgressIndicator(
+                      backgroundColor: Colors.blue[100],
+                      minHeight: 10,
+                      value: 0.5,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("yg"),
+                          DotsIndicator(
+                            dotsCount: 6,
+                            position: 3,
+                          ),
+                          Text('-${formatDuration(160)}')
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    InkWell(
+                      onTap: () {
+                        // if (state is WorkoutInProgress) {
+                        //   BlocProvider.of<WorkoutCubit>(context).pauseWorkout();
+                        // } else if (state is WorkoutPaused) {
+                        //   BlocProvider.of<WorkoutCubit>(context)
+                        //       .resumeWorkout();
+                        // }
+                      },
+                      child: Stack(
+                        alignment: Alignment(0, 0),
+                        children: [
+                          const Center(
+                            child: SizedBox(
+                              height: 220,
+                              width: 220,
+                              child: CircularProgressIndicator(
+                                valueColor:
+                                    AlwaysStoppedAnimation<Color>(Colors.blue),
+                                strokeWidth: 25,
+                                value: 0.3,
+                              ),
+                            ),
+                          ),
+                          Center(
+                            child: SizedBox(
+                              height: 300,
+                              width: 300,
+                              child: Padding(
+                                padding: EdgeInsets.only(bottom: 20),
+                                child: Image.asset('assets/stopwatch.png'),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ));
+        },
+        listener: (context, state) {});
+  }
+}
